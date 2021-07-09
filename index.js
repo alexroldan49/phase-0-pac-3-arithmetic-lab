8aaf7c4372dfc405b4fbae8c1e665f0ebd9c3cec
Alexander Roldan <alexroldan49@gmail.com>
2021-07-09 02:52:43 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){
+    1+1
+}
+function subtract(){
+ 
+    5-4
+}
+function multiply(){
+    5*5
+}
+function divide(){
+    200/10
+}
+function add(a, b) {
+  return a + b
+} 
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return (a / b)
+}
+function increment(n) {
+    return ++n
+}
+function decrement(n) {
+    return --n
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
